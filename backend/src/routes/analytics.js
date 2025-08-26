bf13324a0aac645acc392731be8e1c78932fe196
Nikhilsethi7668 <nikhilsethin494@gmail.com>
2025-08-26 11:06:31 +0530

    updated

--- backend/src/routes/analytics.js
+++ backend/src/routes/analytics.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { getCompanySummary , listCompanyPolicies } from "../controllers/analytics.controller.js";
+import {
+  getCompanySummary,
+  listCompanyPolicies,
+} from "../controllers/analytics.controller.js";
 import protect from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/company/:companyId", protect, getCompanySummary);
-router.get("/company/companyId" , protect, listCompanyPolicies);
+router.get("/company/:companyId/policies", protect, listCompanyPolicies);
 
 export default router;
